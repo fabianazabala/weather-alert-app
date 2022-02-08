d60728c274a9ab98292d129405c9b401a09e42af
fabiana.zabala <fabianasusejzabalag>
2022-02-08 18:41:28 +0100

    Implemented navigation drawer

--- app/src/main/kotlin/com/climate_dissertation_app/ui/MainActivity.kt
+++ app/src/main/kotlin/com/climate_dissertation_app/ui/MainActivity.kt
@@ -1,33 +1,66 @@
 package com.climate_dissertation_app.ui
 
 
-import android.content.Intent
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.MenuItem
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.climate_dissertation_app.R
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_layout.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar_main,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawer_layout.addDrawerListener(
+            toggle
+        )
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
         toHomeActivity()
     }
 
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun toHomeActivity() {
-        supportActionBar?.hide()
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 1500)
+//        supportActionBar?.hide()
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 1500)
     }
 }
